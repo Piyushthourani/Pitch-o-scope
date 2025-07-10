683b345dd639039603f9d918656d8cf7def8865c
Piyushthourani <thouranipiyush@gmail.com>
2025-07-10 21:42:54 +0530

    Components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Slides } from './components/slides/slides';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'slides', component: Slides },
+  { path: '**', redirectTo: '' }  // fallback route
+];
